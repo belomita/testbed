b97db94bed16ae4063ee899e2731f26c9dc6848b
belomita <wenhl5656@163.com>
2013-01-08 15:56:53 +0800

    first commit

---
+++ gen_table.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"time"
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const (
+	STRING_LEN int = 8
+	LINE_NUM int = 1000000
+	COL_NUM int = 6
+)
+func main() {
+	t1 := time.Now()
+	slice := make([]byte, STRING_LEN)
+	cols := make([]string, COL_NUM)
+	//lines := make([]string, LINE_NUM)
+	for line := 1; line < LINE_NUM; line++ {
+		for col := 0; col < COL_NUM; col++ {
+			for index := 0; index < STRING_LEN; index++ {
+				slice[index] = byte(rand.Intn(3)) + 'a'
+			}
+			cols[col] = string(slice)
+		}
+		//lines[line] = strings.Join(cols, "\t")
+		fmt.Println(strings.Join(cols, "\t"))
+	}
+	
+	t2 := time.Now()
+	fmt.Fprintf(os.Stderr, "Time: %v.", t2.Sub(t1))
+}
---
+++ sort_table.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"os"
+	"sort"
+	"time"
+	"bufio"
+	"strings"
+	"fmt"
+)
+
+type Row []string
+type Table struct {
+	Rows  []Row
+	Keys  []int
+}
+
+func (t Table) Len() int { return len(t.Rows) }
+func (t Table) Swap(i, j int) { t.Rows[i], t.Rows[j] = t.Rows[j], t.Rows[i] }
+func (t Table) Less(i, j int) bool { 
+	first, second := t.Rows[i], t.Rows[j]
+	for _, k := range t.Keys {
+		if first[k] != second[k] {
+			return first[k] < second[k]
+		}
+	}
+	return false
+}
+
+func main() {
+	t1 := time.Now()
+	
+	filename := "table.txt"
+	keys := []int{0, 1, 2}
+	f, _ := os.Open(filename)
+	defer f.Close()
+	r := bufio.NewReader(f)
+	
+	table := new(Table)
+	table.Keys = keys
+	// Read from file
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		cols := strings.Split(line, "\t")
+		table.Rows = append(table.Rows, cols)
+	}
+	// sort
+	sort.Sort(table)
+	t2 := time.Now()
+	fmt.Fprintf(os.Stderr, "Time: %v.", t2.Sub(t1))
+	// output
+	for _, v := range table.Rows {
+		fmt.Printf(strings.Join(v, "\t"))
+	}
+	
+
+}
+
+
+
+
+
+
